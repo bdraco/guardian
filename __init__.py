da61cf660a0eb99d3eac395049b4b8e4284fb125
J. Nick Koston <nick@koston.org>
2022-07-18 09:36:36 -0500

    wip

--- __init__.py
+++ __init__.py
@@ -177,7 +177,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
     # Set up all of the Guardian entity platforms:
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
 
     @callback
     def hydrate_with_entry_and_client(func: Callable) -> Callable:
